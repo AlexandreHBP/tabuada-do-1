91150632ed7c2df2893fafd1a5086a64ff2649a8
AlexandreHBP <alehbp2204@gmail.com>
2022-09-17 14:10:40 -0300

    a

--- tabuada.js
+++ tabuada.js
@@ -3,7 +3,7 @@
  * @author Alexandre Henrique
  */
 
-for (var i = 1; i < 11; i++){
+for (var i = 1; i < 101; i++){
  document.write("<p>Tabuada do " + i + "</p>");
  for(var j = 1; j < 11; j++){
     document.write(i + " x " + j + " = " + ( i * j) + "<br>");
