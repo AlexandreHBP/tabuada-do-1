2ae9aa4efc6301c71a2aa919c7e7dd522a745a7b
AlexandreHBP <alehbp2204@gmail.com>
2022-09-17 14:09:22 -0300

    arrumando tudo

--- tabuada.js
+++ tabuada.js
@@ -2,9 +2,8 @@
  * JS - Aula7: Tabuada 
  * @author Alexandre Henrique
  */
-var valor = 7;
 
-for (var i = 10; i < 101; i++){
+for (var i = 1; i < 11; i++){
  document.write("<p>Tabuada do " + i + "</p>");
  for(var j = 1; j < 11; j++){
     document.write(i + " x " + j + " = " + ( i * j) + "<br>");
